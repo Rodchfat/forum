76b984d418976f386d55bd076412d9cf033cc2df
Rod c <raadce@gmail.com>
2016-09-11 02:51:21 +0000

    add styling to the application

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -18,6 +18,28 @@ class CommentsController < ApplicationController
         end
     end
     
+    def destroy
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.find(params[:id])
+        @comment.destroy
+        redirect_to post_path(@post)
+        
+    end
+    
+    def edit
+     @post = Post.find(params[:post_id])   
+    end
+    
+    def update
+       @post = Post.find(params[:post_id]) 
+       if @post.comments.update(comment_params)
+           redirect_to post_path(@post)
+       else
+           render 'edit'
+       end
+        
+    end
+    
     
     
     private
